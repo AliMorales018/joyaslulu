61993da8abd805e5fb9f045edd1cd2553d442190
AliMorales018 <alimoralesmio@gmail.com>
2021-04-27 14:34:00 -0500

    margen izquierdo en miniaturas03

--- components/ProductImage.js
+++ components/ProductImage.js
@@ -40,7 +40,7 @@ function ProductImage({ images }) {
             images.map((imgItem, index) => (
               <button
                 key={index}
-                className="ml-8 relative h-12 w-20 flex-shrink-0 rounded-sm focus:outline-none"
+                className="mx-5 relative h-12 w-20 flex-shrink-0 rounded-sm focus:outline-none"
                 onClick={() => setMainImg(imgItem.node)}
               >
                 <Image
