20d3d26daabcf04460e5c47db8d5fdb946416b1e
AliMorales018 <alimoralesmio@gmail.com>
2021-04-27 00:05:04 -0500

    modificando ancho de miniatura18

--- components/ProductImage.js
+++ components/ProductImage.js
@@ -40,7 +40,7 @@ function ProductImage({ images }) {
             images.map((imgItem, index) => (
               <button
                 key={index}
-                className="relative w-12 mx-4 h-12  flex-shrink-0 rounded-sm focus:outline-none"
+                className="relative w-10 mx-4 h-12  flex-shrink-0 rounded-sm focus:outline-none"
                 onClick={() => setMainImg(imgItem.node)}
               >
                 <Image
