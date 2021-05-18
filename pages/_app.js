9d2bc2f8646b65c373bb5564ce194cc34e08a202
AliMorales018 <alimoralesmio@gmail.com>
2021-05-17 22:36:30 -0500

    Añadido pixel facebook

--- pages/_app.js
+++ pages/_app.js
@@ -2,6 +2,22 @@ import Layout from '@/components/Layout'
 import SEO from '@/components/SEO'
 import '@/styles/globals.css'
 
+/* function FacebookPixel() {
+  React.useEffect(() => {
+    import("react-facebook-pixel")
+      .then((x) => x.default)
+      .then((ReactPixel) => {
+        ReactPixel.init('785931672069383');
+        ReactPixel.pageView();
+
+        Router.events.on("routeChangeComplete", () => {
+          ReactPixel.pageView();
+        });
+      });cla
+  });
+  return null;
+} */
+
 function MyApp({ Component, pageProps }) {
 
   return (
